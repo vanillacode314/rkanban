94789e1fd524f5c7bd693b278d56f3048e3ee014
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-09-02 17:41:00 +0530

    fix meta tag

--- apps/app/src/entry-server.tsx
+++ apps/app/src/entry-server.tsx
@@ -9,7 +9,7 @@ export default createHandler(() => (
 					<meta charset="utf-8" />
 					<meta
 						name="viewport"
-						content="width=device-width, initial-scale=1; interactive-widget=resizes-content"
+						content="width=device-width, initial-scale=1, interactive-widget=resizes-content"
 					/>
 					<meta name="description" content="The best way to manage your projects" />
 					<link rel="manifest" href="/manifest.webmanifest" />
