18f4ea7ee4b929c1ef1fced762b92f858639d2ba
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-09-03 01:54:26 +0530

    fix clipboard overflow

--- apps/app/src/routes/(root).tsx
+++ apps/app/src/routes/(root).tsx
@@ -1,12 +1,8 @@
-import {
-	ColorModeProvider,
-	ColorModeScript,
-	cookieStorageManagerSSR
-} from '@kobalte/core/color-mode';
+import { ColorModeProvider, cookieStorageManagerSSR } from '@kobalte/core/color-mode';
 import { createConnectivitySignal } from '@solid-primitives/connectivity';
 import { Title } from '@solidjs/meta';
 import { createAsync, useBeforeLeave, useLocation, useNavigate } from '@solidjs/router';
-import { For, JSXElement, Match, Show, Switch, createEffect, onMount, untrack } from 'solid-js';
+import { For, JSXElement, Match, Show, Switch, createEffect, untrack } from 'solid-js';
 import { isServer } from 'solid-js/web';
 import { toast } from 'solid-sonner';
 import { getCookie } from 'vinxi/http';
@@ -20,63 +16,6 @@ import { cn } from '~/lib/utils';
 import { getUser } from '~/utils/auth.server';
 import { idb } from '~/utils/idb';
 
-function AutoImportModals() {
-	const modals = import.meta.glob('~/components/modals/auto-import/*.tsx', {
-		eager: true,
-		import: 'default'
-	}) as Record<string, () => JSXElement>;
-
-	return <For each={Object.values(modals)}>{(Modal) => <Modal />}</For>;
-}
-function getServerCookies() {
-	'use server';
-	const colorMode = getCookie('kb-color-mode');
-	return colorMode ? `kb-color-mode=${colorMode}` : '';
-}
-function Clipboard() {
-	const [appContext, setAppContext] = useApp();
-
-	return (
-		<Show when={appContext.clipboard.length > 0}>
-			<Card class="fixed bottom-5 right-5">
-				<CardHeader>
-					<CardTitle>Clipboard</CardTitle>
-				</CardHeader>
-				<CardContent class="flex w-full max-w-sm flex-col gap-2">
-					<For each={appContext.clipboard}>
-						{(item) => (
-							<Switch>
-								<Match when={item.type === 'id/node'}>
-									<Button class="flex items-center gap-2" as="div" variant="secondary">
-										<span
-											class={cn(
-												(item.meta as { node: TNode; path: string }).path.endsWith('.project') ?
-													'i-heroicons:document'
-												:	'i-heroicons:folder'
-											)}
-										/>
-										<span>{(item.meta as { node: TNode; path: string }).path}</span>
-
-										<span
-											class={cn(
-												item.mode === 'move' ? 'i-heroicons:scissors' : 'i-heroicons:clipboard'
-											)}
-										/>
-									</Button>
-								</Match>
-							</Switch>
-						)}
-					</For>
-				</CardContent>
-				<CardFooter>
-					<Button class="w-full" onClick={() => setAppContext('clipboard', [])}>
-						Clear
-					</Button>
-				</CardFooter>
-			</Card>
-		</Show>
-	);
-}
 const RootLayout = (props: { children: JSXElement }) => {
 	const user = createAsync(() => getUser());
 	createEffect(() => {
@@ -111,5 +50,70 @@ const RootLayout = (props: { children: JSXElement }) => {
 		</ColorModeProvider>
 	);
 };
+
+function AutoImportModals() {
+	const modals = import.meta.glob('~/components/modals/auto-import/*.tsx', {
+		eager: true,
+		import: 'default'
+	}) as Record<string, () => JSXElement>;
+
+	return <For each={Object.values(modals)}>{(Modal) => <Modal />}</For>;
+}
+
+function getServerCookies() {
+	'use server';
+	const colorMode = getCookie('kb-color-mode');
+	return colorMode ? `kb-color-mode=${colorMode}` : '';
+}
+
+function Clipboard() {
+	const [appContext, setAppContext] = useApp();
+
+	return (
+		<Show when={appContext.clipboard.length > 0}>
+			<Card class="fixed bottom-5 right-5">
+				<CardHeader>
+					<CardTitle>Clipboard</CardTitle>
+				</CardHeader>
+				<CardContent class="flex w-full max-w-sm flex-col gap-2">
+					<For each={appContext.clipboard}>
+						{(item) => (
+							<Switch>
+								<Match when={item.type === 'id/node'}>
+									<Button
+										class="grid grid-cols-[auto_1fr_auto] items-center gap-2"
+										as="div"
+										variant="secondary"
+									>
+										<span
+											class={cn(
+												(item.meta as { node: TNode; path: string }).path.endsWith('.project') ?
+													'i-heroicons:document'
+												:	'i-heroicons:folder'
+											)}
+										/>
+										<span class="truncate">
+											{(item.meta as { node: TNode; path: string }).path}
+										</span>
+										<span
+											class={cn(
+												item.mode === 'move' ? 'i-heroicons:scissors' : 'i-heroicons:clipboard'
+											)}
+										/>
+									</Button>
+								</Match>
+							</Switch>
+						)}
+					</For>
+				</CardContent>
+				<CardFooter>
+					<Button class="w-full" onClick={() => setAppContext('clipboard', [])}>
+						Clear
+					</Button>
+				</CardFooter>
+			</Card>
+		</Show>
+	);
+}
 
 export default RootLayout;
