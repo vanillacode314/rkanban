0272dcbc6bfa1a8981a6d2bcd8e84b0046d7c490
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-09-15 00:54:03 +0530

    fix: don't use embedded db while building

--- apps/app/src/db.ts
+++ apps/app/src/db.ts
@@ -3,17 +3,27 @@ import { drizzle } from 'drizzle-orm/libsql';
 import env from './utils/env/server';
 import { ms } from './utils/ms';
 
-const client =
-	env.TURSO_SYNC_URL !== undefined ?
-		createClient({
-			url: env.TURSO_CONNECTION_URL,
-			authToken: env.TURSO_AUTH_TOKEN,
-			syncUrl: env.TURSO_SYNC_URL,
-			syncInterval: ms('5 min') / 1000
-		})
-	:	createClient({
-			url: env.TURSO_CONNECTION_URL,
-			authToken: env.TURSO_AUTH_TOKEN
-		});
+const IS_BUILDING = process.env.TURSO_BUILDING !== undefined;
+
+let client;
+if (env.TURSO_SYNC_URL !== undefined) {
+	client =
+		!IS_BUILDING ?
+			createClient({
+				url: env.TURSO_CONNECTION_URL,
+				authToken: env.TURSO_AUTH_TOKEN,
+				syncUrl: env.TURSO_SYNC_URL,
+				syncInterval: ms('5 min') / 1000
+			})
+		:	createClient({
+				url: env.TURSO_SYNC_URL,
+				authToken: env.TURSO_AUTH_TOKEN
+			});
+} else {
+	client = createClient({
+		url: env.TURSO_CONNECTION_URL,
+		authToken: env.TURSO_AUTH_TOKEN
+	});
+}
 
 export const db = drizzle(client);
