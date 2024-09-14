40afae00b0c5d778ca0111ee29f22a97a956b4a7
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-09-14 19:24:27 +0530

    add support for embedded replicas

--- apps/app/src/db.ts
+++ apps/app/src/db.ts
@@ -1,10 +1,19 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import env from './utils/env/server';
+import { ms } from './utils/ms';
 
-const client = createClient({
-	url: env.TURSO_CONNECTION_URL,
-	authToken: env.TURSO_AUTH_TOKEN
-});
+const client =
+	env.TURSO_SYNC_URL !== undefined ?
+		createClient({
+			url: env.TURSO_CONNECTION_URL,
+			authToken: env.TURSO_AUTH_TOKEN,
+			syncUrl: env.TURSO_SYNC_URL,
+			syncInterval: ms('5 min') / 1000
+		})
+	:	createClient({
+			url: env.TURSO_CONNECTION_URL,
+			authToken: env.TURSO_AUTH_TOKEN
+		});
 
 export const db = drizzle(client);
--- apps/app/src/utils/env/server.ts
+++ apps/app/src/utils/env/server.ts
@@ -5,6 +5,7 @@ const env = createEnv({
 	server: {
 		TURSO_AUTH_TOKEN: z.string().min(1),
 		TURSO_CONNECTION_URL: z.string().url(),
+		TURSO_SYNC_URL: z.string().url().optional(),
 		AUTH_SECRET: z.string().min(1),
 		RESEND_API_KEY: z.string().min(1),
 		NOTIFICATIONS_EMAIL_ADDRESS: z.string().min(1)
