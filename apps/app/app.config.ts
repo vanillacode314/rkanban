92b276dc3f516cef137e66b7071edc8b34fd6cfa
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-09-16 18:24:10 +0530

    chore: add vite bundle analyzer

--- apps/app/app.config.ts
+++ apps/app/app.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@solidjs/start/config';
 import devtools from 'solid-devtools/vite';
 import { presetIcons, presetWebFonts } from 'unocss';
 import Unocss from 'unocss/vite';
+import { analyzer } from 'vite-bundle-analyzer';
 import clientEnv from './src/utils/env/client';
 import serverEnv from './src/utils/env/server';
 
@@ -38,7 +39,8 @@ export default defineConfig({
 						}
 					})
 				]
-			})
+			}),
+			analyzer({ analyzerPort: 8889 })
 		]
 	}
 });
