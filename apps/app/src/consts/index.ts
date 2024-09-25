fe62914c7b0228699371eacfd20584390a832a56
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-09-25 06:20:24 +0530

    refactor(app): add more paths to reserved paths list

--- apps/app/src/consts/index.ts
+++ apps/app/src/consts/index.ts
@@ -2,6 +2,12 @@ import { ms } from '~/utils/ms';
 
 const ACCESS_TOKEN_EXPIRES_IN_SECONDS = ms('10 min') / 1000;
 const REFRESH_TOKEN_EXPIRES_IN_SECONDS = ms('1 year') / 1000;
-const RESERVED_PATHS = ['/settings'] satisfies string[];
+const RESERVED_PATHS = [
+	'/settings',
+	'/auth/signin',
+	'/auth/signup',
+	'/auth/forgot-password',
+	'/auth/reset-password'
+] satisfies string[];
 
 export { ACCESS_TOKEN_EXPIRES_IN_SECONDS, REFRESH_TOKEN_EXPIRES_IN_SECONDS, RESERVED_PATHS };
