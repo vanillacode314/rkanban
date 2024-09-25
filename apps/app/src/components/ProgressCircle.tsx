feab6e0faf214583ddb635eaafd84ad1f2e72f45
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-09-25 11:08:55 +0530

    refactor(app): simplify ProgressCircle component

--- apps/app/src/components/ProgressCircle.tsx
+++ apps/app/src/components/ProgressCircle.tsx
@@ -1,37 +1,33 @@
-import { createMemo, Show } from 'solid-js';
-
 import { cn } from '~/lib/utils';
-import { round } from '~/utils/math';
+
+const RADIUS = 9;
+const CIRCUMFERENCE = 2 * Math.PI * RADIUS;
 
 export const ProgressCircle = (props: { class?: string; text?: string; value: number }) => {
-	const radius = 9;
-	const angle = createMemo(() => Math.PI * 2 * Math.min(props.value, 0.999));
-	const large = createMemo(() => (angle() > Math.PI ? 1 : 0));
-	const x = createMemo(() => round(-Math.cos(angle()) * radius + 12, 2));
-	const y = createMemo(() => round(-Math.sin(angle()) * radius + 12, 2));
-	const circumference = createMemo(() => angle() * radius);
-
 	return (
 		<div class={cn('relative', props.class)}>
 			<span class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[.5em] font-bold">
 				{props.text}
 			</span>
-			<svg height="1em" style={{ height: '1em', width: '1em' }} viewBox="0 0 24 24" width="1em">
-				<Show when={props.value > 0}>
-					<path
-						d={`M 3 12 
-          A 9 9 
-          0 ${large()} 1 
-          ${x()} ${y()}
-        Z`}
-						fill="none"
-						stroke="currentColor"
-						stroke-dasharray={circumference().toString()}
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-					/>
-				</Show>
+			<svg
+				class="-rotate-90"
+				height="1em"
+				style={{ height: '1em', width: '1em' }}
+				viewBox="0 0 24 24"
+				width="1em"
+			>
+				<circle
+					cx="12"
+					cy="12"
+					fill="none"
+					r="9"
+					stroke="currentColor"
+					stroke-dasharray={CIRCUMFERENCE.toString()}
+					stroke-dashoffset={((1 - props.value) * CIRCUMFERENCE).toString()}
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+				/>
 			</svg>
 		</div>
 	);
