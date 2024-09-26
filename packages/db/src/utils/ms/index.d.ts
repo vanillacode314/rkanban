51bf9db5d98dec01b2772b36be1a47cdbc073eb9
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-09-26 18:46:32 +0530

    feat: setup cron jobs to clean database

---
+++ packages/db/src/utils/ms/index.d.ts
@@ -0,0 +1,3 @@
+import ms from './_';
+export { ms };
+//# sourceMappingURL=index.d.ts.map
--- packages/db/tsup.config.ts
+++ packages/db/tsup.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-	entryPoints: ['src/**/*.ts'],
-	outdir: 'dist',
-	format: 'esm',
-	outbase: 'src',
-	bundle: true,
-	platform: 'neutral',
-	minify: !options.watch,
-	sourcemap: true,
-	clean: true,
-	onSuccess: 'tsc --emitDeclarationOnly --declaration'
+  entryPoints: ["src/**/*.ts"],
+  outdir: "dist",
+  format: "esm",
+  outbase: "src",
+  bundle: true,
+  platform: "neutral",
+  minify: !options.watch,
+  sourcemap: true,
+  clean: true,
+  onSuccess: "tsc --emitDeclarationOnly --declaration",
 }));
