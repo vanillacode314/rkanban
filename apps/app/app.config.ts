6f388289a432e3a65a228da7de1b49d087d41d07
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-12-14 20:47:14 +0530

    chore(deps): update pdnd

--- apps/app/app.config.ts
+++ apps/app/app.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@solidjs/start/config';
 import devtools from 'solid-devtools/vite';
 import { presetIcons, presetWebFonts } from 'unocss';
 import Unocss from 'unocss/vite';
+
 //import { analyzer } from 'vite-bundle-analyzer';
 import clientEnv from './src/utils/env/client';
 import serverEnv from './src/utils/env/server';
@@ -12,13 +13,11 @@ clientEnv;
 serverEnv;
 
 export default defineConfig({
-	ssr: false,
 	server: {
 		prerender: {
 			routes: ['/offline', '/manifest.webmanifest']
 		}
 	},
-	devOverlay: true,
 	vite: {
 		envPrefix: 'PUBLIC_',
 		plugins: [
@@ -30,8 +29,8 @@ export default defineConfig({
 				presets: [
 					presetIcons({
 						extraProperties: {
-							display: 'inline-block',
 							color: 'auto',
+							display: 'inline-block',
 							'vertical-align': 'middle'
 						}
 					}),
