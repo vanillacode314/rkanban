ef4469cb28573b8b4a62e55426feebb3afc6cd2b
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2025-02-27 23:09:28 +0530

    fix: Fap action types

--- apps/app/src/components/Fab.tsx
+++ apps/app/src/components/Fab.tsx
@@ -5,7 +5,7 @@ import { cn } from '~/lib/utils';
 import { Button } from './ui/button';
 
 export type TAction = {
-	handler: () => Promise<void> | void;
+	handler: () => Promise<unknown> | unknown;
 	icon: string;
 	label: string;
 	variant?: Parameters<typeof Button>[0]['variant'];
