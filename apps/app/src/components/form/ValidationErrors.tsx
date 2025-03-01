06334bb8870da34d90611356a443f0f152105471
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2025-03-01 20:38:13 +0530

    refactor: allow undefined prop for ValidationErrors component

--- apps/app/src/components/form/ValidationErrors.tsx
+++ apps/app/src/components/form/ValidationErrors.tsx
@@ -1,8 +1,8 @@
 import { For, Show } from 'solid-js';
 
-export function ValidationErrors(props: { errors: string[] }) {
+export function ValidationErrors(props: { errors: string[] | undefined }) {
 	return (
-		<Show when={props.errors.length > 0}>
+		<Show when={props.errors && props.errors.length > 0}>
 			<div class="flex flex-col">
 				<For each={props.errors}>
 					{(error) => <span class="text-sm text-error-foreground">{error}</span>}
